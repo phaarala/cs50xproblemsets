73e8856a5228550c5bc771f738bfa986875030e5
Paul Haarala <82416838+phaarala@users.noreply.github.com>
2026-06-17 17:23:09 -0600

    Final scrabble.c
    Fixed calculate_score issue: player_input[i] - 'a' gave the index, but needed to use that to index into letter_values to get the accurate score info.

--- problemset2/scrabble/scrabble.c
+++ problemset2/scrabble/scrabble.c
@@ -1,9 +1,15 @@
-// Implement a program that determines the winner of a short Scrabble-like game. Your program should prompt for input twice: once for "Player 1" and once for "Player 2". Then, depending on which player scores the most points, your program should either print "Player 1 wins!", "Player 2 wins!", or "Tie!" if the score is the same for both players. 
+// Implement a program that determines the winner of a short Scrabble-like game. Your program should
+// prompt for input twice: once for "Player 1" and once for "Player 2". Then, depending on which
+// player scores the most points, your program should either print "Player 1 wins!", "Player 2
+// wins!", or "Tie!" if the score is the same for both players.
 
+#include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-#include <cs50.h>
+
+int letter_values[26] = {1, 3, 3, 2,  1, 4, 2, 4, 1, 8, 5, 1, 3,
+                         1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int calculate_score(string player_input);
 
@@ -18,9 +24,9 @@ int main(void)
     int player2_score = calculate_score(player2);
 
     // Compare scores and print results
-    if (player1_score == player2_score)
+    if (player1_score < player2_score)
     {
-        printf("Tie!\n");
+        printf("Player 2 wins!\n");
     }
     else if (player1_score > player2_score)
     {
@@ -29,26 +35,32 @@ int main(void)
 
     else
     {
-        printf("Player 2 wins!\n");
+        printf("Tie!\n");
     }
-    
+
     // Successfully runs
-    return 0; 
+    return 0;
 }
 
 // Calculate score for a provided string
 int calculate_score(string player_input)
 {
     // Scrabble scoring letter values sequentially from A-Z (ASCII values 65 - 90)
-    int letter_values[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     int word_length = strlen(player_input);
-    int player_score = 0; 
+    int player_score = 0;
 
     // Determines player score by subtracting 65 (ASCII A value) from each provided letter
-    for(int i = 0; i<word_length; i++)
+    for (int i = 0; i < word_length; i++)
     {
-        player_score += player_input[i] - 65;
+        if (isupper(player_input[i]))
+        {
+            player_score += letter_values[player_input[i] - 'A'];
+        }
+        else if (islower(player_input[i]))
+        {
+            player_score += letter_values[player_input[i] - 'a'];
+        }
     }
+
     return player_score;
 }
-
