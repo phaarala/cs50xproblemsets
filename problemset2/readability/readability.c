02bb1be343a50412dc6808c4bab7df22ed314b21
Paul Haarala <82416838+phaarala@users.noreply.github.com>
2026-06-18 15:37:52 -0600

    Update readability.c
    Adjusted types for variables to account for calculation at the end; Coleman-Liau index has been adjusted to be more accurate, round to the nearest integer, and return the value as a string. Now need to figure out how to account for "Before Grade 1" in the output

--- problemset2/readability/readability.c
+++ problemset2/readability/readability.c
@@ -1,6 +1,6 @@
-// Implement a program that calculates the approximate grade level needed to comprehend some text. 
-// Your program should print as output “Grade X” where “X” is the grade level computed, rounded to the nearest integer. 
-// If the grade level is 16 or higher (equivalent to or greater than a senior undergraduate reading level), your program should output “Grade 16+” instead of giving the exact index number. 
+// Implement a program that calculates the approximate grade level needed to comprehend some text.
+// Your program should print as output “Grade X” where “X” is the grade level computed, rounded to the nearest integer.
+// If the grade level is 16 or higher (equivalent to or greater than a senior undergraduate reading level), your program should output “Grade 16+” instead of giving the exact index number.
 // If the grade level is less than 1, your program should output “Before Grade 1”.
 
 // index = 0.0588 * L - 0.296 * S - 15.8 (L = average number of letters per word and S = average number of sentences per 100 words
@@ -18,9 +18,9 @@ int main(void)
 
     // Initialize letter, word, and sentence variables
     int text_length = strlen(text);
-    int letters = 0;
-    int words = 0;
-    int sentences = 0;
+    float letters = 0;
+    float words = 0;
+    float sentences = 1;
 
     for (int i = 0; i < text_length; i++)
     {
@@ -30,18 +30,34 @@ int main(void)
             letters ++;
         }
          // Count the number of words
-        if (isblank(text[i]))
+        else if (isblank(text[i]))
         {
             words++;
         }
         // Count the number of sentences
-        if (ispunct(i))
+        else if (ispunct(text[i]))
         {
             sentences ++;
         }
     }
-    // Compute the Coleman-Liau Index (0.0588 * L - 0.296 * S - 15.8) 
-    long cl_index = 0.0588 * letters - 0.296 * sentences - 15.8
+
+    // Compute the Coleman-Liau Index (0.0588 * L - 0.296 * S - 15.8)
+    float L = 100 * (letters / words);
+    float S = 100 * (sentences / words);
+
+    float cl_index = 0.0588 * L - 0.296 * S - 15.8;
+    string grade_level = round(cl_index);
+
+    // Edge cases
+    if (grade_level < 1)
+    {
+        grade_level = "Before Grade 1\n";
+    }
+    else if (grade_level > 16)
+    {
+        grade_level = "16+";
+    }
 
     // Print the grade level to the user
+    printf("Grade %s\n", grade_level);
 }
